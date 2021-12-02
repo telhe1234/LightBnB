e053c305922014b0ae7eb7cd08c27a6845f6bf50
Taha Helwani <telhe008@uottawa.ca>
2021-12-02 02:30:12 +0000

    Implmented the filtering properties list functionality

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -124,14 +124,58 @@ const getAllProperties = function(options, limit = 10) {
   //   limitedProperties[i] = properties[i];
   // }
   // return Promise.resolve(limitedProperties);
-  return pool
-    .query(`
-      SELECT * FROM properties
-      LIMIT $1;
-      `,
-      [limit])
-    .then((result) => result.rows)
-    .catch((err) => err.message);
+  // return pool
+  //   .query(`
+  //     SELECT * FROM properties
+  //     LIMIT $1;
+  //     `,
+  //     [limit])
+  //   .then((result) => result.rows)
+  //   .catch((err) => err.message);
+
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(options.minimum_price_per_night));
+    queryString += `AND cost_per_night >= $${queryParams.length} * 100 `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(options.maximum_price_per_night));
+    queryString += `AND cost_per_night <= $${queryParams.length} * 100 `;
+  }
+  queryString += `GROUP BY properties.id `;
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 }
 exports.getAllProperties = getAllProperties;
 
